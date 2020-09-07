de9765198f8c363655380ad9e13ae2675e73d4e6
Antonio Cruz <antoniocruznb@gmail.com>
2020-09-06 22:18:20 -0300

    Add files via upload

---
+++ Phyton/Introdução.py
@@ -0,0 +1,20 @@
+#Aula 001
+
+             #IMPRIMINDO COISAS NA TELA
+print("Olá, Mundo!") #Imprime a msg na tela
+print(7 + 4) #imprime o resultado da operação na tela
+print('7'+'4') #junta 7 e 4 imprimindo 74 na tela
+
+             #VARIÁVEIS E SEUS VALORES
+
+nome = 'Neto' #a variavel "nome" recebe o nome Neto
+idade = 19 #idade recebe 19
+peso = 61.5 #peso recebe 61.5
+print(nome,idade,peso)
+
+#LENDO VALORES E ATRIBUINDO A VARIAVEIS
+
+nome1 = input('Qual é o seu nome?')
+idade1 = input('Quantos anos você tem?')
+peso1 = input('Qual o seu peso?')
+print(nome1, idade1, peso1)
